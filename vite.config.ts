40e614ca55f16f038397a410bd6e130f08515807
Gon <ivanriosarts@gmail.com>
2026-09-25 16:26:03 -0300

    Configure Cloudflare D1 database

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "ef51e1bd-972b-4cfd-8845-be3092b349fd";
 
 const { d1, r2 } = hostingConfig;
 
